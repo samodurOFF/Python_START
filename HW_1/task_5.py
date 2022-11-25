85ff90b0b00159d178446f38fbc98e595f4c4e40
Alexander Diakov <alex.dkv@yandex.ru>
2022-11-25 16:11:16 +0300

    task_5 done

--- HW_1/task_5.py
+++ HW_1/task_5.py
@@ -11,3 +11,11 @@
 11 5
 9.22
 """
+
+x1 = int(input("Введите x1: "))
+y1 = int(input("Введите y1: "))
+x2 = int(input("Введите x2: "))
+y2 = int(input("Введите y2: "))
+
+dist = round(((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5, 2)
+print(dist)
