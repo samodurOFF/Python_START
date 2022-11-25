70d2bf59103bb5416bad706ddc4a471140804db8
Artem <artem_1285@mail.ru>
2022-11-25 12:21:50 +0500

    task_4rech

--- HW_1/task_4.py
+++ HW_1/task_4.py
@@ -10,3 +10,16 @@
 3
 x < 0, y < 0
 """
+print("Введите номер четверти от 1 до 4")
+num = int(input())
+if num >= 1 and num <= 4:
+    if num == 1:
+        print(' x > 0 and y > 0')
+    if num == 2:
+        print(' x < 0 and y > 0')
+    if num == 3:
+        print('x < 0 and y < 0')
+    if num == 4:
+        print('x > 0 and y < 0')
+else:
+    print("Введите корректное значение номера четверти")
