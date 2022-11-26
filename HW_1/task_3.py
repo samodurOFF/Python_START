223e217e0e82bc2eb7dcb9d5289dc6da2b7a14fc
svetaepc <40168757+svetaepc@users.noreply.github.com>
2022-11-26 19:16:02 +0300

    Update task_3.py

--- HW_1/task_3.py
+++ HW_1/task_3.py
@@ -19,3 +19,33 @@
 0
 Точка на отрицательной части оси абсцисс
 """
+def inputKoord(x):
+    a = [0] * x
+    for i in range(x):
+        is_OK = False
+        while not is_OK:
+            try:
+                number = float(input(f"Введите {i+1} координату: "))
+                a[i] = number
+                is_OK = True
+                if a[i] == 0:
+                    is_OK = False
+                    print("Координата не должно быть равна 0 ")
+            except ValueError:
+                print("Ты ошибся. Вводить надо числа!")
+    return a
+
+
+def checkCoordinates(xy):
+    text = 4
+    if xy[0] > 0 and xy[1] > 0:
+        text = 1
+    elif xy[0] < 0 and xy[1] > 0:
+        text = 2
+    elif xy[0] < 0 and xy[1] < 0:
+        text = 3
+    print(f"Точка находится в {text} четверти плоскости")
+
+
+koordinate = inputKoord(2)
+checkCoordinates(koordinate)
