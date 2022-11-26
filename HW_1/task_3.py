805f005c9353d460ed8ea16ef1a2045e3bbeab1c
svetaepc <40168757+svetaepc@users.noreply.github.com>
2022-11-26 19:34:08 +0300

    Update task_3.py

--- HW_1/task_3.py
+++ HW_1/task_3.py
@@ -19,33 +19,19 @@
 0
 Точка на отрицательной части оси абсцисс
 """
-def inputKoord(x):
-    a = [0] * x
-    for i in range(x):
-        is_OK = False
-        while not is_OK:
-            try:
-                number = float(input(f"Введите {i+1} координату: "))
-                a[i] = number
-                is_OK = True
-                if a[i] == 0:
-                    is_OK = False
-                    print("Координата не должно быть равна 0 ")
-            except ValueError:
-                print("Ты ошибся. Вводить надо числа!")
-    return a
+def detect_coordinate_quarter(x,y):
+    if((x>0)and(y>0)):
+        res="первая четверть"
+        print(res)
+    if((x<0)and(y>0)):
+        res = "вторая четверть"
+        print(res)
+    if((x<0)and(y<=0)):
+        res = "третья четверть"
+        print(res)
+    if((x>0)and(y<0)):
+        res = "четвертая четверть"
+        print(res)
+    return res
 
-
-def checkCoordinates(xy):
-    text = 4
-    if xy[0] > 0 and xy[1] > 0:
-        text = 1
-    elif xy[0] < 0 and xy[1] > 0:
-        text = 2
-    elif xy[0] < 0 and xy[1] < 0:
-        text = 3
-    print(f"Точка находится в {text} четверти плоскости")
-
-
-koordinate = inputKoord(2)
-checkCoordinates(koordinate)
+detect_coordinate_quarter(34,-30)
