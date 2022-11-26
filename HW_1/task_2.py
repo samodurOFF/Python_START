107826756a647e5f5525e1709e7d96270c6c1333
svetaepc <40168757+svetaepc@users.noreply.github.com>
2022-11-26 19:15:19 +0300

    Update task_2.py

--- HW_1/task_2.py
+++ HW_1/task_2.py
@@ -3,3 +3,24 @@
 
 Вывод: единственное значение типа bool (True либо False)
 """
+def inputNumbers(x):
+    xyz = ["X", "Y", "Z"]
+    a = []
+    for i in range(x):
+        a.append(input(f"Введите значение {xyz[i]}: "))
+    return a
+
+
+def checkPredicate(x):
+    left = not (x[0] or x[1] or x[2])
+    right = not x[0] and not x[1] and not x[2]
+    result = left == right
+    return result
+
+
+statement = inputNumbers(3)
+
+if checkPredicate(statement) == True:
+    print(f"Утверждение истинно")
+else:
+    print(f"Утверждение ложно")
