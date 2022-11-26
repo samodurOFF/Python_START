636144733d57bb480f3205fed5f69eb330da8d4f
svetaepc <40168757+svetaepc@users.noreply.github.com>
2022-11-26 19:31:51 +0300

    Update task_4.py

--- HW_1/task_4.py
+++ HW_1/task_4.py
@@ -10,3 +10,36 @@
 3
 x < 0, y < 0
 """
+
+def detect_coordinate_quarter(x,y):
+    if((x>0)and(y>0)):
+        res="первая четверть"
+        print(res)
+    if((x<0)and(y>0)):
+        res = "вторая четверть"
+        print(res)
+    if((x<0)and(y<=0)):
+        res = "третья четверть"
+        print(res)
+    if((x>0)and(y<0)):
+        res = "четвертая четверть"
+        print(res)
+    return res
+  
+ def detect_coordinate_range(x):
+    if(x == 1):
+        res="(x>0)and(y>0)"
+        print(res)
+    if(x == 2):
+        res = "(x<0)and(y>0)"
+        print(res)
+    if(x == 3):
+        res = "(x<0)and(y<=0)"
+        print(res)
+    if(x == 4):
+        res = "(x>0)and(y<0)"
+        print(res)
+    return res
+  
+  detect_coordinate_range(3)
+  
