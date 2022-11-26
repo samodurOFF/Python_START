45a38afd1cc0fe2eb2ae6a177ce01072dc6ab9e9
svetaepc <40168757+svetaepc@users.noreply.github.com>
2022-11-26 19:13:30 +0300

    Update task_1.py

--- HW_1/task_1.py
+++ HW_1/task_1.py
@@ -15,3 +15,25 @@ True
 1
 False
 """
+def InputNumbers(inputText):
+    is_OK = False
+    while not is_OK:
+        try:
+            number = int(input(f"{inputText}"))
+            is_OK = True
+        except ValueError:
+            print("Это не число!")
+    return number
+
+
+def checkNumber(num):
+    if 6 <= num <= 7:
+        print("Yes")
+    elif 0 < num < 6:
+        print("No")
+    else:
+        print("число вне пределов 7 дней")
+
+
+num = InputNumbers("Введите число: ")
+checkNumber(num)
