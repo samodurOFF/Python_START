921bbe53cdffac449d4fc2437cd89bf52deaa536
Inav Zakharchenko <zakharka2000@ya.ru>
2022-11-27 18:32:59 +0300

    Task_1 финал

--- HW_2/task_1.py
+++ HW_2/task_1.py
@@ -17,30 +17,30 @@ import math
 number = float(input("Введите вещественное число: "))
 
 # Вариант 1
-number_str = str(number)
-summa = 0
-for num in number_str:
-    if num.isdigit():
-        summa += int(num)
+summa = sum([int(num) for num in str(number) if num.isdigit()])
 print(f"Сумма чисел (вариант 1): {summa}")
 
-# Вариант 2
-number_2 = number
-while number_2 != int(number_2):
-    number_2 *= 10
-summa = 0
-while number_2 > 0:
-    summa += number_2 % 10
-    number_2 //= 10
-print(f"Сумма чисел (вариант 2): {int(summa)}")
+# Варианты 2 и 3 работают очень не стабильно из-за неточностей при операциях с плавающей запятой
 
-# Вариант 3
-while number != math.trunc(number):
-    number *= 10
-number = int(number)
-count = int(math.log10(number)) + 1
-summa = 0
-for _ in range(count):
-    summa += number % 10
-    number //= 10
-print(f"Сумма чисел (вариант 3): {summa}")
+# # Вариант 2
+# number_2 = number
+# while number_2 != int(number_2):
+#     number_2 *= 10
+# summa = 0
+# while number_2 > 0:
+#     summa += number_2 % 10
+#     number_2 //= 10
+# print(f"Сумма чисел (вариант 2): {int(summa)}")
+#
+# # Вариант 3
+# number_3 = number
+# while number_3 != math.trunc(number_3):
+#     number_3 *= 10
+# number_3 = int(number_3)
+# count = int(math.log10(number_3)) + 1
+# summa = 0
+# for _ in range(count):
+#     summa += number_3 % 10
+#     number_3 //= 10
+# print(f"Сумма чисел (вариант 3): {summa}")
+
