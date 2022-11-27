811a478daf5f43e88c2f26d0d6f6e9a0f30e01b3
Inav Zakharchenko <zakharka2000@ya.ru>
2022-11-27 17:21:21 +0300

    Task_5 выполнен

--- HW_2/task_4.py
+++ HW_2/task_4.py
@@ -6,3 +6,18 @@
 Ввод: значение типа <int>
 Вывод: значение типа <int>
 """
+
+from random import randint
+
+number = int(input("Введите натуральное число: "))
+my_list = []
+for _ in range(number):
+    my_list.append(randint(-number, number))
+res = 1
+with open('indexes.txt', 'r') as file:
+    for line in file:
+        index = int(line)
+        if len(my_list) > index >= -len(my_list):
+            res *= my_list[index]
+
+print("ответ", res)
