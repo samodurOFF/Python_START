854990c67f13b4b567c5468753d46d43d7a3848c
Darya Piskareva <miragedd@gmail.com>
2022-11-27 19:09:55 +0300

    my work 2

--- HW_1/task_2.py
+++ HW_1/task_2.py
@@ -1,5 +1,10 @@
 """
-Докажите, что выражение ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z истинно для всех значений предикат.
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+# if not (x or y or z) == (not x and not y and not z):
 
-Вывод: единственное значение типа bool (True либо False)
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if not (x or y or z) == (not x and not y and not z):
+                print(f'Утверждение ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z истенно при x = {x}, y={y}, z={z}')
 """
