6f768f79a2bd684aa1721c315fc6e96540e1b8da
«Smirnova <>
2022-11-28 17:38:01 +0300

    Решение задач

--- HW_1/task_5.py
+++ HW_1/task_5.py
@@ -11,12 +11,12 @@
 #11 5
 #9.22
 #"""
+ax = float(input('Введите координаты точки a по оси x:'))
+ay = float(input('Введите координаты точки a по оси y:'))
+bx = float(input('Введите координаты точки b по оси x:'))
+by = float(input('Введите координаты точки b по оси y:'))
 
-float = float(input('Введите координаты точки a по оси x:'))
-float = float(input('Введите координаты точки a по оси y:'))
-float = float(input('Введите координаты точки b по оси x:'))
-float = by(input('Введите координаты точки b по оси y:'))
+import math
+distans = math.sqrt((ax-bx)**2+(ay-by)**2)
+print(f'Растояние между точкой A до точки B = {distans}' )
 
-import  математики
-=   расстояния.sqrt sqrt((ax-bx)**2+(ay-by)**2)
-print(f'Растояние между точкой A до точки B = {distans}'
