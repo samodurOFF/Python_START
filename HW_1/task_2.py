75b1e081480ba37869ae15c14c7dc68bbb6f63e4
Natalya Ch <n_babaeva@mail.ru>
2022-11-28 07:16:32 +0300

    домашнее задание 1

--- HW_1/task_2.py
+++ HW_1/task_2.py
@@ -3,3 +3,9 @@
 
 Вывод: единственное значение типа bool (True либо False)
 """
+result = []
+for x in [True, False]:
+    for y in [True, False]:
+        for z in [True, False]:
+            result.append(not (x or y or z) == (not x and not y and not z))
+print(all(result))
--- HW_1/task_4.py
+++ HW_1/task_4.py
@@ -10,3 +10,12 @@
 3
 x < 0, y < 0
 """
+n = int(input('Введите номер четверти '))
+if n == 1:
+    print("x > 0, y > 0")
+elif n == 2:
+    print("x < 0, y > 0")
+elif n == 3:
+    print("x < 0, y < 0")
+elif n == 4:
+    print("x > 0, y < 0")
