f2d0e0a738580af7573d2dc5172ed3d4e8c1046e
Inav Zakharchenko <zakharka2000@ya.ru>
2022-11-29 22:33:28 +0300

    Task_3 решен

--- HW_3/task_3.py
+++ HW_3/task_3.py
@@ -9,3 +9,11 @@
 [1.1, 1.2, 3.1, 5, 10.01]
 2.0
 """
+from random import uniform
+from math import modf
+
+number = int(input("Введите натуральное число: "))
+my_list = [round(uniform(1, 10), 2) for _ in range(number)]
+print(my_list)
+frac_list = [modf(el)[0] for el in my_list]
+print(round(max(frac_list) - min(frac_list), 2) * 100)
