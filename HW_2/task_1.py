c842c23dc005022b48c1e10be77058a367fd23ca
AntonGoli <fongoli@yandex.ru>
2022-11-29 19:20:20 +0300

    Задача_1

--- HW_2/task_1.py
+++ HW_2/task_1.py
@@ -11,3 +11,18 @@
 0.56
 11
 """
+
+x = input('Введите число ')
+
+
+def summa(x):
+    if float(x) < 0:
+        x = float(x) * (-1)
+    number = 0
+
+    for i in str(x):
+        if i != '.':
+            number += int(i)
+    return number
+print(f'Сумма чисел равна {summa(x)}')
+
