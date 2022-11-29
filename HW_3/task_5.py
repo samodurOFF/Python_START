88af1e9123987ea4b7ee58d18b532b878f9f86fd
Inav Zakharchenko <zakharka2000@ya.ru>
2022-11-29 23:36:35 +0300

    Task_5 доработан

--- HW_3/task_5.py
+++ HW_3/task_5.py
@@ -10,3 +10,12 @@ https://ru.wikipedia.org/wiki/Негафибоначчи
 8
 [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 """
+
+number = int(input("Введите натуральное число: "))
+my_list = [0 for _ in range(number * 2 + 1)]
+my_list[number + 1] = my_list[number - 1] = 1
+for i in range(number - 1):
+    my_list[number + 2 + i] = my_list[number + 1 + i] + my_list[number + i]
+    my_list[number - 2 - i] = my_list[number + 2 + i] * ((-1) ** (i + 1))
+
+print(my_list)
