53fa5463a8418c07165dfa39f7c640a08012eb82
olga <yavruyano90@mail.ru>
2022-11-29 21:10:56 +0300

    Homework

---
+++ HW2/Task2.py
@@ -0,0 +1,14 @@
+# Напишите программу, которая принимает на вход натуральное число N и выдает список факториалов по основаниям от 1 до N
+
+N = int(input('Введите число: '))
+
+def getlist():
+    total = 1
+    current = 1
+    while True:
+        total = total * current
+        yield total
+        current = current + 1
+
+f = getlist()
+print([next(f) for i in range(N)])
