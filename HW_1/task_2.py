c642db3b33508580c1a7bf36703d260030156db3
AntonGoli <fongoli@yandex.ru>
2022-11-29 18:34:52 +0300

    Испавлена задача

--- HW_1/task_2.py
+++ HW_1/task_2.py
@@ -3,8 +3,9 @@
 
 Вывод: единственное значение типа bool (True либо False)
 """
-for x in range(2):
-        for y in range(2):
-            for z in range(2):
-                print(not (x or y or z) == (not x and not y and not z))
-                print(x, y, z)
+result = []
+for x in [True,False]:
+    for y in [True, False]:
+        for z in [True, False]:
+            result.append(not (x or y or z) == (not x and not y and not z))
+print(all(result))
