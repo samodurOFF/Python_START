50ae139d25a5223248a54bcaf7567004b7e88179
Karina <Karinanifontova06@mail.ru>
2022-11-30 16:08:22 +0300

    HW_3/task_1

--- HW_3/task_1.py
+++ HW_3/task_1.py
@@ -11,3 +11,8 @@
 [5, 1, 5, 2, 7, 11]
 14
 """
+import random
+new_list = [random.randint(1, 99)
+            for r in range(int(input('Введите размер списка: ')))]
+print('Сумма чисел с нечетными индексами в списке ', new_list, 'равна',
+      sum(new_list[i] for i in range(len(new_list)) if i % 2 != 0))
