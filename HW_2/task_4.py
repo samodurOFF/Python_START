8629f1a0e25ed873c075d0cdb467decbe5101a12
Irina <1303irina1993lagutina@mail.ru>
2022-12-01 22:05:30 +0700

    Сохранение задания4

--- HW_2/task_4.py
+++ HW_2/task_4.py
@@ -6,6 +6,21 @@
 Ввод: значение типа <int>
 Вывод: значение типа <int>
 """
+
+import random
+num = int(input('Введите число: '))
+new_list = [random.randint(-num, num) for r in range(num)]
+index_list = []
+prod = 1 
+with open('indexes.txt', encoding='UTF-8') as data:
+    for index in data:
+        index_list.append(int(index))
+for i in range(-num, num):
+    if i in index_list:
+        prod *= new_list[i]
+print(prod)
+
+
 import pathlib
 from pathlib import Path
 path = Path("indexes.txt") 
