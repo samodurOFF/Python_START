2840b7bdad582c3a1118e4407c07399098b2fe51
Inav Zakharchenko <zakharka2000@ya.ru>
2022-12-04 15:51:39 +0300

    Task_2 готово

--- HW_4/task_2.py
+++ HW_4/task_2.py
@@ -8,3 +8,22 @@
 [1, 1, 2, 3, 3, 4, 5, 5, 6, 7, 7, 8, 9, 9]
 [2, 4, 6, 8]
 """
+
+my_list = [1, 1, 2, 3, 3, 4, 5, 5, 6, 7, 7, 8, 9, 9]
+
+
+def del_el(x_list, el):
+    while True:
+        try:
+            x_list.remove(el)
+        except ValueError:
+            return x_list
+
+
+i = 0
+while i < len(my_list):
+    if my_list.count(my_list[i]) > 1:
+        my_list = del_el(my_list, my_list[i])
+    else:
+        i += 1
+print(my_list)
