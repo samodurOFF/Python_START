b7c657ff36f70c2bea3dabbf2c9c515312adb76f
Inav Zakharchenko <zakharka2000@ya.ru>
2022-12-04 17:19:29 +0300

    Task_3 готово

--- HW_4/task_3.py
+++ HW_4/task_3.py
@@ -10,3 +10,42 @@
 2
 2x^2 + 4x + 5 = 0
 """
+
+from random import randrange
+import os.path
+
+k = int(input("Введите натуральное число k "))
+polynom = [randrange(101) for i in range(k + 1)]
+
+str_polynom = ""  # создание полинома
+plus = False
+for num in polynom:
+    if num:
+        if plus:
+            str_polynom += " + "
+        else:
+            plus = True
+
+        if k > 1:
+            str_polynom += f"{num}x^{k}"
+        elif k == 1:
+            str_polynom += f"{num}x"
+        else:
+            str_polynom += f"{num}"
+    k -= 1
+str_polynom += " = 0"
+
+print(str_polynom)  # Вывод в консоль
+
+if not os.path.isdir("polynom"):  # Запись в файл
+    os.mkdir("polynom")
+os.chdir("polynom")
+i = 0
+while True:
+    if not os.path.isfile(f"polynom_{i}.txt"):
+        with open(f"polynom_{i}.txt", 'w') as file:
+            file.write(str_polynom)
+        print(f"Результат записан в файл polynom_{i}.txt")
+        break
+    else:
+        i += 1
