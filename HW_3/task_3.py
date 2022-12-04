f7aed93500062232879b6720a15c28a05e5e7ebf
Karina <Karinanifontova06@mail.ru>
2022-12-04 19:49:59 +0300

    HW_3/task_3

--- HW_3/task_3.py
+++ HW_3/task_3.py
@@ -9,3 +9,15 @@
 [1.1, 1.2, 3.1, 5, 10.01]
 2.0
 """
+import random
+new_list = [round(random.uniform(1, 99), 2)
+            for r in range(int(input('Введите размер списка: ')))]
+dec_list = []
+for i in range(len(new_list)):
+    num = str(new_list[i]).split('.')
+    if 1 == len(num[-1]):
+        dec_list.append(int((num[-1])+'0'))
+    else:
+        dec_list.append(int(num[-1]))
+print(new_list)
+print(max(dec_list) - min(dec_list))
