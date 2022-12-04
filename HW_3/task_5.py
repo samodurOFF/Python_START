2430d26c8de60c50197ce729223a6cfefc49d2bc
Karina <Karinanifontova06@mail.ru>
2022-12-04 18:58:38 +0300

    HW_3/task_5

--- HW_3/task_5.py
+++ HW_3/task_5.py
@@ -10,3 +10,11 @@ https://ru.wikipedia.org/wiki/Негафибоначчи
 8
 [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 """
+num = int(input('Введите размерность: '))
+new_list = [1, 0, 1]
+for i in range(num - 1):
+    new_list.append(new_list[-1] + new_list[-2])
+    new_list.insert(0, new_list[1] - new_list[0])
+print(new_list)
+
+  
