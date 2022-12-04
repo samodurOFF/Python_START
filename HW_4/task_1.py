f11b43cad3722c107cca5966744bdbbd738f239f
Inav Zakharchenko <zakharka2000@ya.ru>
2022-12-04 15:18:07 +0300

    Task_1 готово

--- HW_4/task_1.py
+++ HW_4/task_1.py
@@ -11,3 +11,29 @@
 38
 [2, 19]
 """
+
+
+def next_prime(current):
+    """
+    Самый простой и неоптимизированный алгоритм нахождения простого числа
+    """
+    while True:
+        current += 1
+        k = 0
+        for j in range(1, current + 1):
+            if current % j == 0:
+                k += 1
+        if k == 2:
+            return current
+
+
+n = int(input("Введите натуральное число N "))
+mult = 2
+res = []
+while n > 1:
+    if n % mult == 0:
+        res.append(mult)
+        n //= mult
+    else:
+        mult = next_prime(mult)
+print("Список простых множителей натурального числа N: ", res)
