865b90b64e16412f7c8097414b8ea28a7e650ca9
kristina.sigal <kulrikh@gmail.com>
2022-12-07 01:15:41 +0300

    Add HW 2 task 1

--- HW_2/task_1.py
+++ HW_2/task_1.py
@@ -5,9 +5,23 @@
 Вывод: значение типа <int>
 
 Примеры:
-6782.0
-23
+6782.0 -> 23
+0.56 -> 11          """
 
-0.56
-11
-"""
+# number = input('Введите вещественное число ')
+# sum = 0
+# for i in number:
+#     if i.isdigit(): 
+#         sum = sum + int(i)
+# print(f"Сумма цифр в числе {number} равна {sum}")
+
+
+# # 2 var
+# number = ""
+# while (not number.isdecimal()):
+#     number = (input('Введите вещественное число:'))
+# sum = 0
+# for i in number:
+#     if i.isdigit():
+#         sum = sum + int(i)
+# print(f"сумма цифр:{sum}")
