213d9965bc70223798ed568c98bb410d804982a2
Inav Zakharchenko <zakharka2000@ya.ru>
2022-12-06 10:21:18 +0300

    Task_1 готово

--- HW_5/task_1.py
+++ HW_5/task_1.py
@@ -4,3 +4,17 @@
 Ввод: значение типа <str>
 Вывод: значение типа <str>
 """
+
+text = "бамбуковый красный баобабовый синий барбарисовый фиолетовый"
+# Хотел упростить первый вариант, получилось еще два, но проще не стало!
+# Вариант_1
+new_text = " ".join(list(filter(lambda x: not ("а" in x and "б" in x and "в" in x), text.split())))
+print(new_text)
+
+# Вариант_2
+new_text = " ".join(list(filter(lambda x: not all(("а" in x, "б" in x, "в" in x)), text.split())))
+print(new_text)
+
+# Вариант_2
+new_text = " ".join(list(filter(lambda x: not all(map(lambda y, z: z in y, ["абв"], x)), text.split())))
+print(new_text)
