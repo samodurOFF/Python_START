d6f3ce2785d3e5d9e100e09a7b85631321af401a
Alexander Diakov <alex.dkv@yandex.ru>
2022-12-07 22:30:41 +0300

    task_1 done

--- HW_4/task_1.py
+++ HW_4/task_1.py
@@ -11,3 +11,14 @@
 38
 [2, 19]
 """
+
+num = int(input('Введите число N '))
+new_list = []
+divisor = 2
+while divisor <= num:
+    if num % divisor == 0:
+        new_list.append(divisor)
+        num //= divisor
+    else:
+        divisor += 1
+print(new_list)
