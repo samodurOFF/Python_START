b07bc4db3e9f25ff5f0986a1d498f5ab6c202a54
sid1922 <111591366+sid1922@users.noreply.github.com>
2022-12-07 16:17:58 +0300

    task_5

--- HW_3/task_5.py
+++ HW_3/task_5.py
@@ -10,3 +10,18 @@ https://ru.wikipedia.org/wiki/Негафибоначчи
 8
 [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 """
+a = int(input("Введите число "))
+print(a)
+negofibonacci = [1, -1]
+fibonacci = [1, 1]
+
+for i in range(2, a):
+    list = fibonacci[i-1]+fibonacci[i-2]
+    fibonacci.append(list)
+    list_nego = negofibonacci[i-2] - negofibonacci[i-1]
+    negofibonacci.append(list_nego)
+
+negofibonacci.reverse()
+negofibonacci.append(0)
+
+print(f'{negofibonacci+fibonacci}')
