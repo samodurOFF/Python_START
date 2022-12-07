be8adca38d36890ea7b22ac089e20afa70b67de7
Artem Alekseevgit <artem_1285@mail.ru>
2022-12-07 18:53:38 +0500

    решил

--- HW_4/task_1.py
+++ HW_4/task_1.py
@@ -11,3 +11,15 @@
 38
 [2, 19]
 """
+num = int(input('Введите число: '))
+result_list = []
+i = 2
+while i <= num:
+    if num % i == 0:
+        result_list.append(i)
+        num //= i
+    else:
+        i += 1
+if num > 1:
+    result_list.append(num)
+print(result_list)
