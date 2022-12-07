0779881332d0d99e47e167215e95010ff8f735e0
Artem Alekseevgit <artem_1285@mail.ru>
2022-12-07 18:58:37 +0500

    решил

--- HW_4/task_2.py
+++ HW_4/task_2.py
@@ -8,3 +8,14 @@
 [1, 1, 2, 3, 3, 4, 5, 5, 6, 7, 7, 8, 9, 9]
 [2, 4, 6, 8]
 """
+from random import randint
+
+n = int(input("Введите число: "))
+my_list = [randint(0, n) for i in range(n)]
+print(my_list)
+result_list = []
+
+for number in my_list:
+    if number not in result_list:
+        result_list.append(number)
+print(result_list)
