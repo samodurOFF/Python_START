86bac317270d209ba2665eb577fd5e974d33df1a
kristina.sigal <kulrikh@gmail.com>
2022-12-08 22:36:56 +0300

    Edit HW 2 task 3

--- HW_2/task_3.py
+++ HW_2/task_3.py
@@ -7,7 +7,7 @@
 Вывод: значение типа <float>
 
 Пример:
-1
+2
 2.0
 
 2
@@ -16,3 +16,34 @@
 3
 6.62
 """
+
+num = ''
+while not num.isdigit():
+    num = input('Введите целое число N: ')
+
+N = int(num)
+some_list = []
+for i in range(1, N + 1):
+    some_list.append((1 + 1 / i) ** i)
+result = sum(some_list)
+print(round(result, 2))
+
+
+# 2й вариант
+# n = int(input('ВВедите число N: '))
+# some_list = round(sum([(1 + 1 / i) ** i for i in range(1, n + 1)]), 2)
+# print(some_list)
+
+# 3й вариант
+# num = ''
+# while not num.isdigit():
+#     num = input('Введите целое число N: ')
+
+# N = int(num)
+# some_list = []
+# for n in range(1, N + 1):
+#     some_list.append((1 + 1 / n) ** n)
+# sum = 0
+# for i in some_list:
+#     sum = sum + i
+# print(round(sum, 2))
