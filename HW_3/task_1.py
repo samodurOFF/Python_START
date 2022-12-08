a99cc1898c12b36ab43c5793e3a3b10707db8de4
unknown <tvot21@gmail.com>
2022-12-08 21:06:08 +0300

    task1

--- HW_3/task_1.py
+++ HW_3/task_1.py
@@ -11,3 +11,21 @@
 [5, 1, 5, 2, 7, 11]
 14
 """
+
+
+
+from random import  randrange
+n = int(input('Введите количество элементов: '))
+a = [randrange (0, 10) for i in range (n)]
+
+print(a)
+
+def sum_of_position(a):
+    sum = 0
+    for i in range(len(a)):
+        if a[i]%2 != 0:
+            sum += a[i]
+    return sum
+print(f'Сумма элементов на нечетных позициях равна: {sum_of_position(a)}')
+
+
