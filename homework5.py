52779603a6eb3fc7534b5ad7daf194e0913d6cc6
holodilina <112629404+holodilina@users.noreply.github.com>
2022-12-12 00:38:22 +0300

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -2,3 +2,8 @@
 # для всех значений предикат.
 
 
+for x in range(2):
+        for y in range(2):
+            for z in range(2):
+                print(not (x or y or z) == (not x and not y and not z))
+                print(x, y, z)
