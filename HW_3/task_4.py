8448f86d55755a843aefeaa51282ba5a634f7830
unknown <tvot21@gmail.com>
2022-12-11 21:26:49 +0300

    tast4

--- HW_3/task_4.py
+++ HW_3/task_4.py
@@ -14,3 +14,13 @@
 2
 10
 """
+numbernumber = int(input('Введите целое число: '))
+
+def convert_decimal_to_binaryconvert_decimal_to_binary (number):
+    num = ""
+    0 > 0
+
+    num = num(num % 2) + num
+    num //= 2
+    return  num
+print(convert_decimal_to_binaryconvert_decimal_to_binary(number))
