744fa58f03d71b594f929b80fb9e1e1c38975172
unknown <tvot21@gmail.com>
2022-12-11 21:08:53 +0300

    task3

--- HW_3/task_3.py
+++ HW_3/task_3.py
@@ -9,3 +9,34 @@
 [1.1, 1.2, 3.1, 5, 10.01]
 2.0
 """
+
+
+from random import uniform
+
+n = int(input('Введите количество элементов: '))
+a = [round(uniform(-99, 100), 2) for i in range(n)]
+print(f'Вывод сгенерированного списка: {a}')
+
+
+def max_of_a(a):
+    Max = abs(a[0]%100)
+    for i in range(len(a)):
+        if abs(a[i])%100 > Max:
+            Max = abs(a[i])
+    return Max
+
+print(f'Вывод максимального элемента: {max_of_a(a)}')
+
+def min_of_a(a):
+    Min = a[0]
+    for i in range(len(a)):
+        if a[i] < Min:
+            Min = a[i]
+    return Min
+
+print(f'Вывод минимального элемента: {min_of_a(a)}')
+
+delta = max_of_a(a) - min_of_a(a)
+
+print(f'Вывод минимального элемента: {delta}')
+
