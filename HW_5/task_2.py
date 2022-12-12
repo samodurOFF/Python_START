792f78574c70dd37ec49b6f5eeb9e8c657c4615d
«Smirnova <>
2022-12-12 14:19:51 +0300

    Решение задач

--- HW_5/task_2.py
+++ HW_5/task_2.py
@@ -9,3 +9,46 @@
 a) Добавьте игру против бота
 b) Подумайте, как наделить бота простейшим "интеллектом"
 """
+
+from random import randint
+
+def input_dat(name):
+    x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до n: "))
+    while x < 1 or x > n:
+        x = int(input(f"{name}, введите корректное количество конфет: "))
+    return x
+
+
+def p_print(name, k, counter, value):
+    print(f"Ходил {name}, он взял {k}, теперь у него {counter}. Осталось на столе {value} конфет.")
+
+player1 = input("Введите имя первого игрока: ")
+player2 = input("Введите имя бота: ")
+value = int(input("Введите количество конфет на столе: "))
+flag = randint(0,2) # флаг очередности
+if flag:
+    print(f"Первый ходит {player1}")
+else:
+    print(f"Первый ходит {player2}")
+
+counter1 = 0 
+counter2 = 0
+
+while value > 28:
+    if flag:
+        k = input_dat(player1)
+        counter1 += k
+        value -= k
+        flag = False
+        p_print(player1, k, counter1, value)
+    else:
+        k = input_dat(player2)
+        counter2 += k
+        value -= k
+        flag = True
+        p_print(player2, k, counter2, value)
+
+if flag:
+    print(f"Выиграл {player1}")
+else:
+    print(f"Выиграл {player2}")
