10a4fa967e57f0e635525f4e3ed348e9c8240b17
unknown <tvot21@gmail.com>
2022-12-16 18:10:10 +0300

    task2

--- HW_4/task_2.py
+++ HW_4/task_2.py
@@ -8,3 +8,13 @@
 [1, 1, 2, 3, 3, 4, 5, 5, 6, 7, 7, 8, 9, 9]
 [2, 4, 6, 8]
 """
+from random import  randrange
+n = int(input('Введите количество элементов: '))
+a = [randrange (0, 10) for i in range (n)]
+print(a)
+new_list = []
+new_list.append(a[0])
+for idx in range(len(a) - 1):
+     if a[idx] != a[idx + 1]:
+        new_list.append(a[idx + 1])
+print(new_list)
