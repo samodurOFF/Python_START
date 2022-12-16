4443bb1ef8a106fb143411bf1c5527dbb7c99d5c
unknown <tvot21@gmail.com>
2022-12-16 17:53:03 +0300

    task1

--- HW_4/task_1.py
+++ HW_4/task_1.py
@@ -11,3 +11,16 @@
 38
 [2, 19]
 """
+
+num = int(input("Введите число: "))
+i = 2 
+list = []
+old = num
+while i <= num:
+    if num % i == 0:
+        list.append(i)
+        num //= i
+        i = 2
+    else:
+        i += 1
+print(f"{old}  {list}")
