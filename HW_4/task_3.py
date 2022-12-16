5e56b10facc55df48032bb0a5aeaf2ebb4973042
unknown <tvot21@gmail.com>
2022-12-16 18:16:12 +0300

    task3

--- HW_4/task_3.py
+++ HW_4/task_3.py
@@ -10,3 +10,44 @@
 2
 2x^2 + 4x + 5 = 0
 """
+import random
+
+
+def write_file(st):
+    with open('file33.txt', 'w') as data:
+        data.write(st)
+
+
+def rnd():
+    return random.randint(0,101)
+
+
+def create_mn(k):
+    lst = [rnd() for i in range(k+1)]
+    return lst
+    
+
+def create_str(sp):
+    lst= sp[::-1]
+    wr = ''
+    if len(lst) < 1:
+        wr = 'x = 0'
+    else:
+        for i in range(len(lst)):
+            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
+                wr += f'{lst[i]}x^{len(lst)-i-1}'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 2 and lst[i] != 0:
+                wr += f'{lst[i]}x'
+                if lst[i+1] != 0:
+                    wr += ' + '
+            elif i == len(lst) - 1 and lst[i] != 0:
+                wr += f'{lst[i]} = 0'
+            elif i == len(lst) - 1 and lst[i] == 0:
+                wr += ' = 0'
+    return wr
+
+k = int(input("Введите натуральную степень k = "))
+koef = create_mn(k)
+write_file(create_str(koef))
