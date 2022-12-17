8140d00d1af586a62a5c63db9db07ad72ee04c2d
Nikita Lachugin <schel2009@yandex.ru>
2022-12-17 20:46:22 +0300

    task4

--- HW_3/task_4.py
+++ HW_3/task_4.py
@@ -14,3 +14,11 @@
 2
 10
 """
+
+int_num = int(input("Введите целое число: "))
+double_num = ''
+while int_num > 0:
+    double_elem =int_num % 2
+    int_num = int_num // 2
+    double_num += str(double_elem) 
+print(int(double_num[::-1]))
