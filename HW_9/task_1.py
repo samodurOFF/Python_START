9daf0417415128f40e432867d6402c399a127657
«Smirnova <>
2023-01-09 17:14:35 +0300

    задача 1

---
+++ HW_9/task_1.py
@@ -0,0 +1,26 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
+
+
+async def binary(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    input_num = update.message.text.split()
+    a = int(input('Enter the number: '))
+    b = ''
+    while a > 0:
+        b = str(a % 2) + b
+        a = a // 2
+    print(b)
+ await update.message.reply_text(f'в двоичной системе число:{b}')
+
+
+
+
+bot_token = ""
+app = ApplicationBuilder().token(bot_token).build()
+
+app.add_handler(CommandHandler("hello", hello))
+app.add_handler(CommandHandler("bin", binary))
+app.run_polling()
