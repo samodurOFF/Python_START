1544754fda372127eebc946e831252840b1be3aa
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-03-07 12:41:03 +0300

    Добавили небольшие правки кода

--- task_28.py
+++ task_28.py
@@ -5,4 +5,19 @@
 # 2 2
 # 4 
 
+def sum(a, b):
+    if a == 0:
+        return b
+    elif b == 0:
+        return a
+    else:
+        return sum(a-1, b+1)
+
+# получаем значения a и b от пользователя
+a = int(input("Введите число a: "))
+b = int(input("Введите число b: "))
+
+# вызываем функцию sum и выводим результат
+result = sum(a, b)
+print(f"Сумма чисел {a} и {b} равна {result}")
 
