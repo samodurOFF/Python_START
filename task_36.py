c385770e9b782a57c58c725a7d68b0f9683bd6e6
Smirnov Mihail <smirnov_mikhael@bk.ru>
2023-03-12 13:41:47 +0300

    Добавили решение 36 задачи

--- task_36.py
+++ task_36.py
@@ -12,27 +12,12 @@
 #  5 10 15 20 25 30
 #  6 12 18 24 30 36
 
-# def print_operation_table(operation, num_rows=6, num_columns=6):
-#     # Выводим шапку таблицы с номерами столбцов
-#     print(" ", end="")
-#     for column in range(1, num_columns+1):
-#         print(column, end=" ")
-#     print()
-#     # Выводим содержимое таблицы
-#     for row in range(1, num_rows+1):
-#         print(row, end=" ")
-#         for column in range(1, num_columns+1):
-#             print(operation(row, column), end=" ")
-#         print()
-
-# # Пример использования функции с операцией умножения
-# print_operation_table(lambda x, y: x * y)
 
 def print_operation_table(operation, num_rows=6, num_columns=6):
-    for i in range(1, num_rows+1):
+    for i in range(1, num_rows + 1):
         row = ""
-        for j in range(1, num_columns+1):
-            row += str(operation(i, j)) + " "
+        for j in range(1, num_columns + 1):
+            row += str(operation(i, j)).rjust(3) + " "
         print(row)
 
 print_operation_table(lambda x, y: x * y)
